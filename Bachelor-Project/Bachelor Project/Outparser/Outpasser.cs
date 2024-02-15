c2aebef53ae81b4bc86a4c735c8686bbdbb6d2d2
Student214933 <s214933@student.dtu.dk>
2024-02-15 18:00:37 +0100

    Outpasser can
    ElectrodeOn and ElectrodeOff are now a thing.

--- Bachelor-Project/Bachelor Project/Outparser/Outpasser.cs
+++ Bachelor-Project/Bachelor Project/Outparser/Outpasser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using static Bachelor_Project.Simulation.Electrode;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,15 @@ namespace Bachelor_Project.Outparser
 {
     static class Outpasser
     {
-        
+        public static void ElectrodeOn(Simulation.Electrode e)
+        {
+            Console.WriteLine("setel " + e.DriverID + " " + e.ElectrodeID + " \\r");
+        }
+
+        public static void ElectrodeOff(Simulation.Electrode e) 
+        {
+            Console.WriteLine("clrel " + e.DriverID + " " + e.ElectrodeID + " \\r");
+        }
+
     }
 }
--- Bachelor-Project/Bachelor Project/obj/Debug/net8.0/Bachelor Project.AssemblyInfo.cs
+++ Bachelor-Project/Bachelor Project/obj/Debug/net8.0/Bachelor Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bachelor Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac04647b7614cbbed14e302b82afec3ceaf0bd70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ef830bf429c195b9d60eafd1a2fcc8378d2f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bachelor Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bachelor Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

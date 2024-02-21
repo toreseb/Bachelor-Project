737495f9a823018bcdc7233baf82d3888dcb30fe
Student214933 <s214933@student.dtu.dk>
2024-02-21 11:28:22 +0100

    Begun implementation of multithreading
    Needs to be started and task queue needs to be set up

--- Bachelor-Project/Bachelor Project/Simulation/Droplet.cs
+++ Bachelor-Project/Bachelor Project/Simulation/Droplet.cs
@@ -6,12 +6,17 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bachelor_Project.Simulation
 {
     internal class Droplet: TileEntity
     {
+        // In creation of a Droplet, the following is needed:
+        // Droplet <name> = new Droplet();
+        // await (?) <name>.StartAgent();
+
 
         public string Substance_Name { get; set; }
         public string Color { get; set; }
@@ -21,6 +26,11 @@ namespace Bachelor_Project.Simulation
         public float Volume { get; set; }
         public ArrayList Occupy { get; set; } = [];
 
+        // Used for threading
+        private CancellationTokenSource cancellationTokenSource;
+        private ManualResetEventSlim workAvailableEvent = new ManualResetEventSlim(false);
+
+
         public Droplet(Input input, float volume, string substance_name, string name = "") : base(input.PositionX, input.PositionY, 1, 1, name)
         {
             Temperature = 20;
@@ -29,6 +39,45 @@ namespace Bachelor_Project.Simulation
             Color = GetColor(Substance_Name);
             Size = getSize(volume);
             Droplet_Actions.InputDroplet(this, input, Size);
+
+            cancellationTokenSource = new CancellationTokenSource();
+
+        }
+
+        public async Task StartAgent()
+        {
+            await Task.Run(() => { RunAgent(cancellationTokenSource.Token); });
+        }
+
+        public async Task RunAgent(CancellationToken cancellationToken)
+        {
+            // Run while cancellation has not been requested
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // Needs to wait for task to be put in a task queue by Commander, then execute task and wait again
+
+                // Wait for work to become available
+                workAvailableEvent.Wait(cancellationToken);
+
+                // Reset signal
+                workAvailableEvent.Reset();
+
+                // Do thing
+
+            }
+        }
+
+        public void GiveWork(Task task)
+        {
+            // TODO: Implement task queue
+
+            // Signal that work is available
+            workAvailableEvent.Set();
+        }
+
+        public void Stop()
+        {
+            cancellationTokenSource.Cancel();
         }
 
         private string GetColor(String substance_name)

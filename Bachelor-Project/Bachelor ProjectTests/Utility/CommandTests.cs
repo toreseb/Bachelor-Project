022b7335e53b0fbe4c337e7db35e9f9a92c43b8b
toreseb <toreseb@gmail.com>
2024-03-01 12:00:04 +0100

    fix prints and command tests

--- Bachelor-Project/Bachelor ProjectTests/Utility/CommandTests.cs
+++ Bachelor-Project/Bachelor ProjectTests/Utility/CommandTests.cs
@@ -36,6 +36,136 @@ namespace Bachelor_Project.Utility.Tests
             Assert.AreEqual(1, d.GetWork().Count);
             Assert.AreEqual(TaskStatus.Created, d.GetWork()[0].Status);
             d.Stop();
+            Program.C.board.Droplets.Remove(d.Name);
         }
+        [TestMethod()]
+        public void ExecuteOutputCommandTest()
+        {
+            Droplet d = new("water", "test");
+            Program.C.board.Droplets.Add("test", d);
+            Command command = new("output", [d.Name], [], "out0");
+            Assert.AreEqual("output", command.Type);
+            command.ExecuteCommand();
+            Assert.AreEqual(1, d.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d.GetWork()[0].Status);
+            d.Stop();
+            Program.C.board.Droplets.Remove(d.Name);
+        }
+
+        [TestMethod()]
+        public void ExecuteWasteCommandTest()
+        {
+            Droplet d = new("water", "test");
+            Program.C.board.Droplets.Add("test", d);
+            Command command = new("waste", [d.Name], []);
+            Assert.AreEqual("waste", command.Type);
+            command.ExecuteCommand();
+            Assert.AreEqual(1, d.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d.GetWork()[0].Status);
+            d.Stop();
+            Program.C.board.Droplets.Remove(d.Name);
+        }
+        [TestMethod()]
+        public void ExecuteMergeCommandTest()
+        {
+            Droplet d1 = new("water", "test1");
+            Droplet d2 = new("water", "test2");
+            Droplet d3 = new("water", "test3");
+            Program.C.board.Droplets.Add("test1", d1);
+            Program.C.board.Droplets.Add("test2", d2);
+            Program.C.board.Droplets.Add("test3", d3);
+            Command command = new("merge", [d1.Name, d2.Name], [d3.Name]);
+            Assert.AreEqual("merge", command.Type);
+            command.ExecuteCommand();
+            Assert.AreEqual(1, d1.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d1.GetWork()[0].Status);
+            Assert.AreEqual(1, d2.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d2.GetWork()[0].Status);
+            Assert.AreEqual(1, d3.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d3.GetWork()[0].Status);
+            d1.Stop();
+            d2.Stop();
+            d3.Stop();
+            Program.C.board.Droplets.Remove(d1.Name);
+            Program.C.board.Droplets.Remove(d2.Name);
+            Program.C.board.Droplets.Remove(d3.Name);
+        }
+        [TestMethod()]
+        public void ExecuteSplitCommandTest()
+        {
+            Droplet d1 = new("water", "test1");
+            Droplet d2 = new("water", "test2");
+            Droplet d3 = new("water", "test3");
+            Program.C.board.Droplets.Add("test1", d1);
+            Program.C.board.Droplets.Add("test2", d2);
+            Program.C.board.Droplets.Add("test3", d3);
+            Command command = new("split", [d1.Name], [d2.Name, d3.Name]);
+            Assert.AreEqual("split", command.Type);
+            command.ExecuteCommand();
+            Assert.AreEqual(1, d1.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d1.GetWork()[0].Status);
+            Assert.AreEqual(1, d2.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d2.GetWork()[0].Status);
+            Assert.AreEqual(1, d3.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d3.GetWork()[0].Status);
+            d1.Stop();
+            d2.Stop();
+            d3.Stop();
+            Program.C.board.Droplets.Remove(d1.Name);
+            Program.C.board.Droplets.Remove(d2.Name);
+            Program.C.board.Droplets.Remove(d3.Name);
+        }
+        [TestMethod()]
+        public void ExecuteMixCommandTest()
+        {
+            Droplet d = new("water", "test");
+            Program.C.board.Droplets.Add("test", d);
+            Command command = new("mix", [d.Name], [d.Name], "square");
+            Assert.AreEqual("mix", command.Type);
+            command.ExecuteCommand();
+            Assert.AreEqual(1, d.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d.GetWork()[0].Status);
+            Command command2 = new("mix", [d.Name], [d.Name], "square", "mixed water");
+            Assert.AreEqual("mix", command2.Type);
+            command2.ExecuteCommand();
+            Assert.AreEqual(2, d.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d.GetWork()[1].Status);
+            d.Stop();
+            Program.C.board.Droplets.Remove(d.Name);
+
+        }
+        [TestMethod()]
+        public void ExecuteTempCommandTest()
+        {
+            Droplet d = new("water", "test");
+            Program.C.board.Droplets.Add("test", d);
+            Command command = new("temp", [d.Name], [d.Name], "20");
+            Assert.AreEqual("temp", command.Type);
+            command.ExecuteCommand();
+            Assert.AreEqual(1, d.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d.GetWork()[0].Status);
+            Command command2 = new("temp", [d.Name], [d.Name], "40", "hot water");
+            Assert.AreEqual("temp", command2.Type);
+            command2.ExecuteCommand();
+            Assert.AreEqual(2, d.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d.GetWork()[1].Status);
+            d.Stop();
+            Program.C.board.Droplets.Remove(d.Name);
+        }
+
+        [TestMethod()]
+        public void ExecuteSenseCommandTest()
+        {
+            Droplet d = new("water", "test");
+            Program.C.board.Droplets.Add("test", d);
+            Command command = new("sense", [d.Name], [d.Name], "water");
+            Assert.AreEqual("sense", command.Type);
+            command.ExecuteCommand();
+            Assert.AreEqual(1, d.GetWork().Count);
+            Assert.AreEqual(TaskStatus.Created, d.GetWork()[0].Status);
+            d.Stop();
+            Program.C.board.Droplets.Remove(d.Name);
+        }
+
     }
 }

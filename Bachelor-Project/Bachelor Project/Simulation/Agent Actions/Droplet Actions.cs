69ad9dca15d41f0fdb832f91b3c6cf994e4cd84e
Student214933 <s214933@student.dtu.dk>
2024-03-15 13:41:56 +0100

    Higher level functions moved from Droplet_Actions to Mission_Tasks

--- Bachelor-Project/Bachelor Project/Simulation/Agent Actions/Droplet Actions.cs
+++ Bachelor-Project/Bachelor Project/Simulation/Agent Actions/Droplet Actions.cs
@@ -13,9 +13,7 @@ namespace Bachelor_Project.Simulation.Agent_Actions
 {
     // This class contains the more basic movements and actions an agent can take.
     public static class Droplet_Actions
-    {
-        private static readonly int mixAmount = 5;
-        
+    {        
         public static void InputDroplet(Droplet d, Input i, int volume, Apparature? destination = null)
         {
             Electrode destElectrode = null;
@@ -178,104 +176,7 @@ namespace Bachelor_Project.Simulation.Agent_Actions
         }
 
 
-        // Droplets needing mixing are assumed to have been merged into one drop.
-        // Does not take contaminants into account yet.
-        public static void Mix(Droplet d) //TODO: Remake to make sure that droplet interference makes it try a different direction, not give up
-        {
-            bool up = true; bool down = true; bool left = true; bool right = true;
-            // Check if there is room to boogie
-            // Only checks board bounderies
-            foreach (Electrode e in d.Occupy)
-            {
-                // Check board bounderies
-                if (e.ePosX < 1) left = false;
-                if (!(e.ePosX < Program.C.board.GetXElectrodes() - 1)) right = false;
-                if (e.ePosY < 1) up = false;
-                if (!(e.ePosY < Program.C.board.GetYElectrodes() - 1)) down = false;
-            }
 
-            // Check for other droplets and contaminants in zone (+ boarder)
-            // Needs to check for each possible direction
-            List<Electrode> temp = new List<Electrode>(d.Occupy);
-
-            if(Convert.ToInt32(up)+ Convert.ToInt32(right)+ Convert.ToInt32(down)+ Convert.ToInt32(left) >= 2 && !((Convert.ToInt32(up) + Convert.ToInt32(down) == 0)||(Convert.ToInt32(right) + Convert.ToInt32(left) == 0)))
-            {
-                foreach (Electrode e in d.Occupy)
-                {
-                    if (up && Program.C.board.Electrodes[e.ePosX, e.ePosY - 1].Occupant != d)
-                    {
-                        temp.Add(Program.C.board.Electrodes[e.ePosX, e.ePosY - 1]);
-                    }
-                    if (right && Program.C.board.Electrodes[e.ePosX + 1, e.ePosY].Occupant != d)
-                    {
-                        temp.Add(Program.C.board.Electrodes[e.ePosX + 1, e.ePosY]);
-                    }
-                    if (down && !up && Program.C.board.Electrodes[e.ePosX, e.ePosY + 1].Occupant != d)
-                    {
-                        temp.Add(Program.C.board.Electrodes[e.ePosX, e.ePosY + 1]);
-                    }
-                    if (left && !right && Program.C.board.Electrodes[e.ePosX - 1, e.ePosY].Occupant != d)
-                    {
-                        temp.Add(Program.C.board.Electrodes[e.ePosX - 1, e.ePosY]);
-                    }
-                }
-                List<Direction> directions = [];
-                if (up)
-                {
-                    directions.Add(Direction.UP);
-                }
-                else
-                {
-                    directions.Add(Direction.DOWN);
-                }
-                if (right)
-                {
-                    directions.Add(Direction.RIGHT);
-                }else
-                {
-                    directions.Add(Direction.LEFT);
-                }
-                if (!up)
-                {
-                    directions.Add(Direction.UP);
-                }else
-                {
-                    directions.Add(Direction.DOWN);
-                }
-                if (!right)
-                {
-                    directions.Add(Direction.RIGHT);
-                }
-                else
-                {
-                    directions.Add(Direction.LEFT);
-                }
-                // Check if area is legal
-                if (CheckLegalMove(d, temp))
-                {
-                    for (int i = 0; i < mixAmount; i++)
-                    {
-                        foreach (var item in directions)
-                        {
-                            MoveDroplet(d, item);
-                            Program.C.board.PrintBoardState();
-                        }
-                    }
-
-                    return;
-                }
-                else
-                {
-                    throw new IllegalMoveException();
-                }
-
-            }
-            else
-            {
-                throw new IllegalMoveException();
-            }
-
-        }
 
         // Used to check if new droplet position upholds border
         private static bool CheckBorder(Droplet d, List<Electrode> temp)
@@ -538,36 +439,6 @@ namespace Bachelor_Project.Simulation.Agent_Actions
         }
 
         internal static void OutputDroplet(Droplet droplet, Output output)
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal static void WasteDroplet(Droplet droplet)
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal static void MergeDroplets(List<string> inputDroplets, Droplet droplet)
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal static void SplitDroplet(Droplet droplet, List<string> outputDroplets)
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal static void MixDroplets(Droplet droplet1, Droplet droplet2, string pattern, string newType)
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal static void TempDroplet(Droplet droplet1, Droplet droplet2, int temp, string newType)
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal static void SenseDroplet(Droplet droplet1, Droplet droplet2, string sensorType)
         {
             //throw new NotImplementedException();
         }
--- Bachelor-Project/Bachelor Project/Simulation/Agent Actions/Mission Tasks.cs
+++ Bachelor-Project/Bachelor Project/Simulation/Agent Actions/Mission Tasks.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bachelor_Project.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,135 @@ namespace Bachelor_Project.Simulation.Agent_Actions
     // This class contains the more complicated missions the agents will have.
     internal class Mission_Tasks
     {
+        private static readonly int mixAmount = 5;
+
+        // Droplets needing mixing are assumed to have been merged into one drop.
+        // Does not take contaminants into account yet.
+        public static void MixDroplets(Droplet d, string pattern, string newType) //TODO: Remake to make sure that droplet interference makes it try a different direction, not give up
+        {
+            bool up = true; bool down = true; bool left = true; bool right = true;
+            // Check if there is room to boogie
+            // Only checks board bounderies
+            foreach (Electrode e in d.Occupy)
+            {
+                // Check board bounderies
+                if (e.ePosX < 1) left = false;
+                if (!(e.ePosX < Program.C.board.GetXElectrodes() - 1)) right = false;
+                if (e.ePosY < 1) up = false;
+                if (!(e.ePosY < Program.C.board.GetYElectrodes() - 1)) down = false;
+            }
+
+            // Check for other droplets and contaminants in zone (+ boarder)
+            // Needs to check for each possible direction
+            List<Electrode> temp = new List<Electrode>(d.Occupy);
+
+            if (Convert.ToInt32(up) + Convert.ToInt32(right) + Convert.ToInt32(down) + Convert.ToInt32(left) >= 2 && !((Convert.ToInt32(up) + Convert.ToInt32(down) == 0) || (Convert.ToInt32(right) + Convert.ToInt32(left) == 0)))
+            {
+                foreach (Electrode e in d.Occupy)
+                {
+                    if (up && Program.C.board.Electrodes[e.ePosX, e.ePosY - 1].Occupant != d)
+                    {
+                        temp.Add(Program.C.board.Electrodes[e.ePosX, e.ePosY - 1]);
+                    }
+                    if (right && Program.C.board.Electrodes[e.ePosX + 1, e.ePosY].Occupant != d)
+                    {
+                        temp.Add(Program.C.board.Electrodes[e.ePosX + 1, e.ePosY]);
+                    }
+                    if (down && !up && Program.C.board.Electrodes[e.ePosX, e.ePosY + 1].Occupant != d)
+                    {
+                        temp.Add(Program.C.board.Electrodes[e.ePosX, e.ePosY + 1]);
+                    }
+                    if (left && !right && Program.C.board.Electrodes[e.ePosX - 1, e.ePosY].Occupant != d)
+                    {
+                        temp.Add(Program.C.board.Electrodes[e.ePosX - 1, e.ePosY]);
+                    }
+                }
+                List<Direction> directions = [];
+                if (up)
+                {
+                    directions.Add(Direction.UP);
+                }
+                else
+                {
+                    directions.Add(Direction.DOWN);
+                }
+                if (right)
+                {
+                    directions.Add(Direction.RIGHT);
+                }
+                else
+                {
+                    directions.Add(Direction.LEFT);
+                }
+                if (!up)
+                {
+                    directions.Add(Direction.UP);
+                }
+                else
+                {
+                    directions.Add(Direction.DOWN);
+                }
+                if (!right)
+                {
+                    directions.Add(Direction.RIGHT);
+                }
+                else
+                {
+                    directions.Add(Direction.LEFT);
+                }
+                // Check if area is legal
+                if (Droplet_Actions.CheckLegalMove(d, temp))
+                {
+                    for (int i = 0; i < mixAmount; i++)
+                    {
+                        foreach (var item in directions)
+                        {
+                            Droplet_Actions.MoveDroplet(d, item);
+                            Program.C.board.PrintBoardState();
+                        }
+                    }
+
+                    return;
+                }
+                else
+                {
+                    throw new IllegalMoveException();
+                }
+
+            }
+            else
+            {
+                throw new IllegalMoveException();
+            }
+
+        }
+
+        internal static void WasteDroplet(Droplet droplet)
+        {
+            //throw new NotImplementedException();
+        }
+
+        internal static void MergeDroplets(List<string> inputDroplets, Droplet droplet)
+        {
+            //throw new NotImplementedException();
+        }
+
+        internal static void SplitDroplet(Droplet droplet, List<string> outputDroplets)
+        {
+            //throw new NotImplementedException();
+        }
+
+        internal static void TempDroplet(Droplet droplet1, Droplet droplet2, int temp, string newType)
+        {
+            //throw new NotImplementedException();
+        }
+
+        internal static void SenseDroplet(Droplet droplet1, Droplet droplet2, string sensorType)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+
     }
 }
